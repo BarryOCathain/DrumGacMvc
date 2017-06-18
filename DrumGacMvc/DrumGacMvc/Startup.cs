00135f0c176ec736955a124244f7def481a3190a
Barry O'Kane <arcangel666@hotmail.co.uk>
2017-06-18 13:03:37 +0100

    Initial Setup, with AngularJS.

---
+++ DrumGacMvc/DrumGacMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DrumGacMvc.Startup))]
+namespace DrumGacMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
